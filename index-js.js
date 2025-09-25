76c7f06275829118fd6ca5de324c09d502a7f4d3
refaldy <refaldybanggana@gmail.com>
2025-09-25 19:42:20 +0700

    creating simulation

--- index-js.js
+++ index-js.js
@@ -1,5 +1,5 @@
-import { createWalletClient, custom, createPublicClient } from 'https://esm.sh/viem@2.9.0'
-// import { mainnet } from 'https://esm.sh/viem@2.9.0/chains'
+import { createWalletClient, custom, createPublicClient, parseEther, defineChain } from 'https://esm.sh/viem@2.9.0'
+import { contractAddress, abi } from './constants-js.js'
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton")
@@ -34,21 +34,44 @@ async function fund(params) {
         walletClient = createWalletClient({
             transport: custom(window.ethereum)
         })
-        await walletClient.requestAddresses()
+        const [connectedAccount] = await walletClient.requestAddresses()
+        const currentChain = await getCurrentChain(walletClient)
 
         publicClient = createPublicClient({
             transport: custom(window.ethereum)
         })
         await publicClient.simulateContract({
-            address: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
-            abi: wagmiAbi,
-            functionName: 'mint',
-            account,
+            address: contractAddress,
+            abi,
+            functionName: 'fund',
+            account: connectedAccount,
+            chain: currentChain,
+            value: parseEther(ethAmount)
         })
     } else {
         connectButton.innerHTML = 'Please install MetaMask!'
     }
 }
+
+async function getCurrentChain(client) {
+    const chainId = await client.getChainId()
+    const currentChain = defineChain({
+        id: chainId,
+        name: "Custom Chain",
+        nativeCurrency: {
+            name: "Ether",
+            symbol: "ETH",
+            decimals: 18,
+        },
+        rpcUrls: {
+            default: {
+                http: ["http://localhost:8545"],
+            },
+        },
+    })
+    return currentChain
+}
+
 
 connectButton.onclick = connect;
 fundButton.onclick = fund
