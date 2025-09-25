90fbb9d7a236089cd5785267b5d457101925c51e
refaldy <refaldybanggana@gmail.com>
2025-09-25 20:58:53 +0700

    sending tracsaction

--- index-js.js
+++ index-js.js
@@ -40,7 +40,7 @@ async function fund(params) {
         publicClient = createPublicClient({
             transport: custom(window.ethereum)
         })
-        await publicClient.simulateContract({
+        const { request } = await publicClient.simulateContract({
             address: contractAddress,
             abi,
             functionName: 'fund',
@@ -48,6 +48,8 @@ async function fund(params) {
             chain: currentChain,
             value: parseEther(ethAmount)
         })
+        const hash = await walletClient.writeContract(request)
+        console.log(hash)
     } else {
         connectButton.innerHTML = 'Please install MetaMask!'
     }
@@ -72,6 +74,7 @@ async function getCurrentChain(client) {
     return currentChain
 }
 
-
 connectButton.onclick = connect;
 fundButton.onclick = fund
+
+// anvil --load-state fundme-anvil.json --host 0.0.0.0 --port 8545
